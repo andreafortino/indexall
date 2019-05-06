c4a01c218bbd0a1fe97632461b60e29385475674
Andrea Fortino <andreas.fortino@gmail.com>
2019-05-06 12:39:00 +0200

    Add files via upload

--- indexall.php
+++ indexall.php
@@ -2,7 +2,7 @@
 
 /* -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=-=-=--=-=-==--=-=-=--=-=-=--=-=-==--=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=
 	author Andrea Fortino <andreas.fortino@gmail.com>
-	this code is downloadable from <http://sourceforge.net/users/andreafortino>
+	this code is downloadable from <https://github.com/andreafortino/indexall>
 	and is under GPL v.2 license <http://www.gnu.org/licenses/gpl-2.0.html>
 	this work is based on (and is an improvement of) original code by Brian Donovan  <http://dev.lophty.com/indexer> 
 
@@ -17505,7 +17505,7 @@ for ($h = 0; $h < count($commonImageFormats); $h++)
 
 print ("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">
 <!-- indexall author Andrea Fortino <andreas.fortino@gmail.com>
-get indexall php source code from <http://sourceforge.net/users/andreafortino>
+get indexall php source code from <https://github.com/andreafortino/indexall>
 indexall is based on (and is an improvement of) original code by Brian Donovan  <http://dev.lophty.com/indexer> -->
 
 <html lang=\"it\">
@@ -17719,10 +17719,10 @@ $listingHTMLstring
 
 <!-- THIS FOOTER MUST NOT BE CANCELLED -->
 <font face=Verdana, Arial, Helvetica, sans-serif size=1><br>*  **  ****  **  *<br>".$ver."<br>
-indexall script author's <a href=\"mailto:andreas.fortino@gmail.com\">mail</a> and <a href=\"http://andreafortino.altervista.org\" target=_blank>website</a><br>
+indexall script author's <a href=\"mailto:andreas.fortino@gmail.com\">mail</a> and <a href=\"https://www.andreafortino.com\" target=_blank>website</a><br>
 
-<a href=\"http://sourceforge.net/users/andreafortino\" target=_blank>php source</a> under <a href=\"http://www.gnu.org/licenses/gpl-2.0.html\" target=_blank>GPL license</a><br>
-based on <a href=\"http://dev.lophty.com/indexer\" target=_blank>lophty directory indexer</a> code<br>
+<a href=\"https://github.com/andreafortino/indexall\" target=_blank>php source</a> under <a href=\"http://www.gnu.org/licenses/gpl-2.0.html\" target=_blank>GPL license</a><br>
+based on <a href=\"https://lophty.com/\" target=_blank>lophty directory indexer</a> code<br>
 see <a href=\"http://www.iconseeker.com/search/filetype/\" target=_blank>iconseeker</a> for links to icons authors' sites<br>
 this php page uses <a href=\"http://php.holtsmark.no/base64img\" target=_blank>base64img script</a></CENTER>
 <BR>
